19406c2e498718ca170e475b0f987a9a82293e5e
Lalithakeerthanagit config --global user.email keerusunkara07@gmail.com <keerusunkara07@gmail.com>
2025-12-10 16:56:47 +0530

    First project upload

---
+++ test.py
@@ -0,0 +1,13 @@
+'''num=int(input("enter a number: "))
+for i in range(1,11):
+    print(f"{num}x{i}={num*i}")'''
+'''rows=int(input("enter no.of rows: "))
+for i in range(1,rows+1):
+    print("*"*i)'''
+n=int(input("enter how many terms you want: "))
+a,b=0,1
+for i in range(n):
+    print(a)
+    a,b=b,a+b
+
+        
