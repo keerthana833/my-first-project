d2302289af3e099d43d9b7a6aeaaccd3ed9c2a57
Lalithakeerthanagit config --global user.email keerusunkara07@gmail.com <keerusunkara07@gmail.com>
2025-12-12 21:32:44 +0530

    removed unwanted files

--- simple projects on if-else/traffic_light simulator.py
+++
@@ -1,9 +0,0 @@
-traffic_light=input("Enter the traffic light colour(red/yellow/green): ").lower()
-if traffic_light=="red":
-    print("STOP!")
-elif traffic_light=="yellow":
-    print("GET READY!")
-elif traffic_light=="green":
-    print("GO!")
-else:
-    print("invalid!please enter the correct one")
